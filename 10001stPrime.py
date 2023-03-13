7d2c8c7736df575090fdd9e139f89cd9cf2f7e07
bluerhodfa <ira.theobold@gmail.com>
2023-03-13 09:27:45 +0000

    added cooments to Problem 7

--- 10001stPrime.py
+++ 10001stPrime.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+"""
+projecteuler.net Problem 7
+
+Find the 10001st prime
+
+Some useful facts:
+1 is not a prime.
+All primes except 2 are odd.
+All primes greater than 3 can be written in the form 6k+/-1.
+n .
+The consequence for primality testing of a number n is: if we cannot find a number f less than
+Any number n can have only one primefactor greater than
+or equal n that divides n then n is prime: the only primefactor of n is n itself
+
+
+
+"""
 import datetime
 import math
 
